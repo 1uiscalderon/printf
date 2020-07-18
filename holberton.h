f6ca9078dd94fcf0b01b58ea5cd316e25b964ba5
1uiscalderon <1857@holbertonschool.com>
2020-07-18 19:01:21 +0000

    Added op var struct

--- holberton.h
+++ holberton.h
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <unistd>
 #include <stdarg.h>
+
 int _putchar(char c);
+typedef struct op
+{
+        char *char; // Character used to print acording to type
+        int (*f)(va_list); // Function taken from the list
+}op_t;
+
 
 #endif
