065aeb7df07092b44a16cd1a82bd0a0a15f975cf
1uiscalderon <1857@holbertonschool.com>
2020-07-19 00:45:05 +0000

    Test

--- _printf.c
+++ _printf.c
@@ -6,48 +6,71 @@
  * @format: All characters to be used acording to user.
  * Return: Characters
  */
-int(*take_form(char signo))(va_list))
+int (*take_form(char signo))(va_list)
 {
-        simb op_list[] = {
-            {"c", print_char},
-            /*   {"s", print_string},
-                {"%", print_%},
-                {"d", print_int},
-                {"i", print_int},
-               */
-            {NULL, NULL}};
-        int i;
+	simb op_list[] = {
+	    {"c", print_char},
+	    {"s", print_string},
+	    {"%", print_% },
+	    {"d", print_int},
+	    {"i", print_int},
+	    {NULL, NULL}};
+	int i = 0;
 
-        for (i = 0; op_list[i].simb != NULL; i++)
-        {
-                if (signo == op_list[i].simb[0])
-                {
-                        return (op_list[i].f);
-                }
-        }
-        return (0);
-                    
+	if (signo == '\0')
+		return (NULL);
+
+	for (i = 0; op_list[i].simb != NULL; i++)
+	{
+		if (signo == op_list[i].simb[0])
+		{
+			return (op_list[i].f);
+		}
+	}
+	return (NULL);
 }
 
 int _printf(const char *format, ...)
 {
-        int i = 0;
-        int (*simb)(va_list);
-        va_list argumentos;
-        
-        va_start(argumentos,format); 
-        while (format[i] != '\0')
-        {
-                
-                
-           i++;
-        }
-        va_end(argumentos);
+	int i = 0, cont = -1;
 
-   /*             return (-1) // Not sure if error or return
+	va_list list;
 
-                    va_start(list, format)
-                    //funcion
-                    va_end(list);
-        return (0) //La funcion de print del character */
+	if (format[i] != '\0')
+	{
+		cont = 0;
+		va_start(list, format);
+		for (i = 0; format[i] != '\0'; i++)
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '\0')
+				{
+					cont = -1;
+					break;
+				}
+				else
+				{
+					simb = take_form(format[i + 1]);
+				}
+				if (simb == NULL)
+				{
+					cont += _putchar('%');
+					cont += _putchar(format[i + 1]);
+				}
+				else
+				{
+					cont += simb(list);
+				}
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				cont++;
+			}
+		}
+		va_end(list);
+	}
+	return(cont);
 }
