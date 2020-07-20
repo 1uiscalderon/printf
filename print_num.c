84de679fc5f3d3d03863883b72a6a4a31d3c77a4
1uiscalderon <1857@holbertonschool.com>
2020-07-20 20:24:13 +0000

    Betty approved

--- print_num.c
+++ print_num.c
@@ -1,31 +1,47 @@
 #include "holberton.h"
+
+/**
+ * print_int - Function used to print integers
+ * @n: va_list parameter
+ * Return: Number of integers printed
+ */
+
 int print_int(va_list n)
 {
-        unsigned int num = 0;
-        int i = 0, count = 1;
-        num = va_arg(n, int);
-        i = num;
-        if (i < 0)
-        {
-                _putchar('-');
-                i = i * -1;
-                num = i;
-                count++;
-        }
-        for (; num >= 10; count++)
-        {
-                num = num / 10;
-        }
-        print_number(i);
-        return (count);
+	unsigned int num = 0;
+	int i = 0, count = 1;
+
+	num = va_arg(n, int);
+
+	i = num;
+
+	if (i < 0)
+	{
+		_putchar('-');
+		i = i * -1;
+		num = i;
+		count++;
+	}
+	for (; num >= 10; count++)
+	{
+		num = num / 10;
+	}
+	print_number(i);
+	return (count);
 }
+
+/**
+ * print_number - Function used to print integers using recursion
+ * @n2: va_list parameter
+ */
 
 void print_number(int n2)
 {
-        int a = n2;
-        if (a / 10)
-        {
-                print_number(a / 10);
-        }
-        _putchar((a % 10) + '0');
+	int a = n2;
+
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+	_putchar((a % 10) + '0');
 }
