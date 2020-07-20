c5e874831ce8a8be451f6e82405515ea4613d082
luiscarvil <1888@holbertonschool.com>
2020-07-20 21:29:22 +0000

    add

--- holberton.h
+++ holberton.h
@@ -7,13 +7,18 @@
 #include <stdio.h>
 
 int _putchar(char c);
+/**
+ * struct simbolo - structure.
+ * @simb: pointer structure.
+ * @f: function pointer.
+ */
 typedef struct simbolo
 {
-        char *simb; 
-        int (*f)(); 
-}simb;
+	char *simb;
+	int (*f)();
+} simb;
 int _printf(const char *format, ...);
-int(*take_form(char signo))(va_list);
+int (*take_form(char signo))(va_list);
 int print_char(va_list c);
 int print_string(va_list s);
 int print_porc(void);
