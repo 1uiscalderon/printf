de4317ad84842ac335cd5f610a4eace694c19322
luiscarvil <1888@holbertonschool.com>
2020-07-21 00:26:00 +0000

    print numb and binary

--- print_num.c
+++ print_num.c
@@ -46,3 +46,48 @@ void print_number(unsigned int n2)
 	}
 	_putchar((a % 10) + '0');
 }
+/**
+ * print_binary - Function used to print integers
+ * @n: va_list parameter
+ * Return: Number of integers printed
+ */
+
+int print_binary(va_list n)
+{
+
+	int num = 0;
+	unsigned int i = 0;
+	int count = 1;
+
+	num = va_arg(n, int);
+	i = num;
+	if (num < 0)
+	{
+		_putchar('1');
+		num = num * -1;
+		i = num;
+		count++;
+	}
+	for (; i > 0; count++)
+	{
+		i = i / 2;
+	}
+	print_number_binary(num);
+	return (count);
+}
+
+/**
+ * print_number_binary - Function used to print integers using recursion
+ * @n2: va_list parameter
+ */
+
+void print_number_binary(int n2)
+{
+	unsigned int a = n2;
+
+	if (a / 2)
+	{
+		print_number_binary(a / 2);
+	}
+	_putchar(a % 2 + '0');
+}
