25e4b506a9c9b9a343f6c05cc21a86b8c013aaf5
luiscarvil <1888@holbertonschool.com>
2020-07-21 00:26:47 +0000

    print numb and binary

--- holberton.h
+++ holberton.h
@@ -24,4 +24,6 @@ int print_string(va_list s);
 int print_porc(void);
 int print_int(va_list n);
 void print_number(unsigned int n2);
+int print_binary(va_list n);
+void print_number_binary(int n2);
 #endif
